9851640b259fe3d7bfaf6bed1f1ea7bdacf9a6b7
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2026-01-30 22:52:47 +0200

    feat: Make Camera follow Player
    Camera needs target to be assigned

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 10, -10);
+    public float smoothSpeed = 10f;
+
+    void Update()
+    {
+        if (!target) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(
+            transform.position,
+            desiredPosition,
+            smoothSpeed * Time.deltaTime
+        );
+
+        transform.LookAt(target);
+    }
+}
