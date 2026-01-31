ea97c478d2235dde1c1c03bfde15d91191e2805c
Swanchick <61909965+Swanchick@users.noreply.github.com>
2026-01-31 16:28:47 +0200

    Added mask

--- Assets/Scripts/GroupMovement.cs
+++ Assets/Scripts/GroupMovement.cs
@@ -53,8 +53,6 @@ public class GroupMovement : PointMovement
         MoveUpdate(groupController, currentMoveToPoint);
         RotateGroup();
 
-        Debug.Log(currentMoveToPointIndex);
-
         if (status == GroupStatus.None)
         {
             status = GroupStatus.Waiting;
